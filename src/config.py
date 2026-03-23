24bd33d8371d3ab1f004fb598bf5d442a8059c85
Austin Pinto <austinpinto@austins-air.home>
2026-03-23 23:49:23 +0000

    Added PostgreSQL integration and environment-based config

--- src/config.py
+++ src/config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -11,3 +14,9 @@ LOG_FILE = os.path.join(BASE_DIR, "logs", "pipeline.log")
 DB_PATH = os.path.join(BASE_DIR, "data", "db", "retail.db")
 
 TABLE_NAME = "retail_data"
+
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
--- src/load.py
+++ src/load.py
@@ -3,6 +3,8 @@ import os
 import logging
 from config import PROCESSED_DATA_PATH, DB_PATH, TABLE_NAME
 import sqlite3
+import psycopg2
+from config import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
 
 logger = logging.getLogger(__name__)
 
@@ -20,9 +22,51 @@ def save_to_sqlite(df: pd.DataFrame) -> None:
         df.to_sql(TABLE_NAME, conn, if_exists="replace", index=False)
 
     logger.info(f"Data successfully loaded into SQLite database: {DB_PATH}")
+ 
+def save_to_postgres(df: pd.DataFrame) -> None:
+    logger.info("Loading data into PostgreSQL")
+
+    conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS retail_data (
+            InvoiceNo TEXT,
+            StockCode TEXT,
+            Description TEXT,
+            Quantity INTEGER,
+            InvoiceDate TIMESTAMP,
+            UnitPrice FLOAT,
+            CustomerID INTEGER,
+            Country TEXT,
+            TotalPrice FLOAT,
+            Year INTEGER,
+            Month INTEGER
+        );
+    """)
+
+    cursor.execute("TRUNCATE TABLE retail_data;")
+
+    for _, row in df.iterrows():
+        cursor.execute("""
+            INSERT INTO retail_data VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """, tuple(row))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    logger.info("Data successfully loaded into PostgreSQL")
 
 def load_data(df: pd.DataFrame) -> None:
     logger.info("Starting load step")
     save_to_csv(df)
     save_to_sqlite(df)
+    save_to_postgres(df)
     logger.info("Load step completed successfully")
